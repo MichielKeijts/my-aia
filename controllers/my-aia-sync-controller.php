ceaf0fef189db56974f9d697c47be4411ca44b05
Michiel <mkeijts@hotmail.com>
2016-11-17 11:47:20 +0100

    Updated Join us & Redirect Link

--- controllers/my-aia-sync-controller.php
+++ controllers/my-aia-sync-controller.php
@@ -309,8 +309,10 @@ class MY_AIA_SYNC_CONTROLLER extends MY_AIA_CONTROLLER {
 		
 		
 		//-- FROM SUGAR TO WORDPRESS
+		
 		// update Wordpress with Sugar profile Data
-		//*if (filter_input(INPUT_POST, 'user_sync')>0 && $items < 100) 
+		//*
+		if (filter_input(INPUT_POST, 'user_sync')>0 && $items < 100) 
 			$items += $this->sync_profiles_sugar_to_wordpress($this->sync_dates['sync_profiles_sugar_to_wordpress']);
 		
 		// update Wordpress with Sugar Event Data
